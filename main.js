d886de822cb049db655b77c660dacc490392c340
LeonardoSarallo <leo.sarallo@gmail.com>
2018-11-27 22:27:34 +0100

    pushfinalecorretto

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $('.hamburger_menu_button').click(function(){
-$(".hamburger_menu").show()});
+  $(".hamburger_menu").show();
+});
 
 $('.close_logo').click(function(){
-$(".hamburger_menu").hide()});
+$(".hamburger_menu").hide()
+});
